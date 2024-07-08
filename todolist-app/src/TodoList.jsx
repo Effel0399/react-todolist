43cfb4719a267cd460b1360c1c0f3748dc3e7742
FNH99 <148479741+FNH99@users.noreply.github.com>
2024-07-08 08:17:12 +0700

    add basic html, set const and function, complere handleInputChange function

--- todolist-app/src/TodoList.jsx
+++ todolist-app/src/TodoList.jsx
@@ -2,6 +2,37 @@ import React, {useState} from 'react'
 
 function TodoList(){
     
-    return(<></>);
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+        setNewTask(event.target.value)
+    }
+    function addTask(){
+
+    }
+    function deleteTask(index){
+
+    }
+    function moveTaskUp(index){
+
+    }
+    function moveTaskDown(index){
+
+    }
+
+    return(
+        <div className='todo-list'>
+            <h1>To-Do-List</h1>
+            
+            <div>
+                <input 
+                type="text"
+                placeholder='Enter a task...'
+                value={newTask}
+                onChange={handleInputChange}/>
+            </div>
+        </div>
+    );
 }
 export default TodoList
