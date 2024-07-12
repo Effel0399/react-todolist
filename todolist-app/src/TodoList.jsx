7a447a67328cc8e7cebcb9dff11423ccfc6e2223
FNH99 <148479741+FNH99@users.noreply.github.com>
2024-07-12 20:45:39 +0700

    change icons

--- todolist-app/src/TodoList.jsx
+++ todolist-app/src/TodoList.jsx
@@ -51,10 +51,10 @@ function TodoList(){
                             delete
                         </button>
                         <button className='move-button' onClick={() => moveTaskUp(index)}>
-                            ⬆️
+                            ⬆
                         </button>
                         <button className='move-button' onClick={() => moveTaskDown(index)}>
-                            ⬇️
+                            ⬇
                         </button>
                     </li>
                 )}
